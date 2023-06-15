7df3f2cec37ade260a961e009c8af4066b1b3e68
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-16 00:10:27 +0530

    updating the "."

--- src/components/Child.js
+++ src/components/Child.js
@@ -10,7 +10,7 @@ const Child = ({modal, displayFun})=>{
                 modal &&
                  (<div>
                     <h3>Modal Content</h3>
-                    <p>This is the modal content</p>
+                    <p>This is the modal content.</p>
                  </div>)
             }
         </div>
